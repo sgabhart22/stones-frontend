dad4b8aa057681fd990583b8114995ecb654d11c
Gabhart Spencer (CLT9GGV) <CLT9GGV@UPS.COM>
2019-06-03 14:38:06 -0400

    Colors defined for wild

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,12 +2,14 @@ const colormap = {
 	'bg' : {
 		'black': '#212121',
 		'red': '#B71C1C',
-		'green': '#1B5E20'
+		'green': '#1B5E20',
+		'wild': '#D7CCC8'
 	},
 	'fg': {
 		'red': '#f44336',
 		'blue': '#2196f3',
-		'green': '#66bb6a'
+		'green': '#66bb6a',
+		'wild': '#FFE082'
 	}
 };
 
