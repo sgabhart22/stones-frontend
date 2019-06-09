325751aafbff3652c0a766e0d08e6b62bbe076f7
Spencer C. Gabhart <sgabhart2230@gmail.com>
2019-06-09 15:04:00 -0400

    Took away deck responsibility and gave it to Game

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -10,23 +10,19 @@ import { StoneDeck } from '../models/stone-deck';
 export class BoardService {
 
 	private board: Board<Stone>;
-	private deck: StoneDeck;
 
-  constructor() { 
+  constructor() {
 		this.board = new Board(10, 10);
-		this.deck = new StoneDeck();
-
-		this.placeAFew();
 	}
 
 	getBoard(): Board<Stone> {
 		return this.board;
 	}
 
-	placeAFew() {
-		this.board.setAt(0, 1, this.deck.pop());
-		this.board.setAt(1, 9, this.deck.pop());
-		this.board.setAt(9, 0, this.deck.pop());
+	placeAFew(deck: StoneDeck) {
+		this.board.setAt(0, 1, deck.pop());
+		this.board.setAt(1, 9, deck.pop());
+		this.board.setAt(9, 0, deck.pop());
 	}
 
 }
