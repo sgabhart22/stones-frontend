3900958d1c08012947344cd5f927db084cd468bd
Spencer C. Gabhart <sgabhart2230@gmail.com>
2019-06-09 14:07:36 -0400

    Added Game to module, shifted layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CellComponent } from './components/cell/cell.component';
 import { BoardComponent } from './components/board/board.component';
 import { QueueComponent } from './components/queue/queue.component';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CellComponent,
     BoardComponent,
-    QueueComponent
+    QueueComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
