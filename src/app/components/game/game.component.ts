573fe56e3ff8f934234f2bcd2518da28ac913649
Gabhart Spencer (CLT9GGV) <CLT9GGV@UPS.COM>
2019-06-18 08:19:50 -0400

    Revised game status check

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -55,10 +55,23 @@ export class GameComponent implements OnInit {
 
 	private checkStatus() {
 		if(!this.game.deck.hasNext()) {
-			console.log('Winner!!!');
+			if(this.isBoardFull()) console.log('Winner!!!');
 		} else if(!this.game.canFit(this.queue[0])) {
 			console.log('Game over...');	
 		}
+	}
+
+	private isBoardFull(): boolean {
+		let filled: number = 0;
+		let board: Board<Stone> = this.getBoard();
+
+		for(var i: number = 0; i < board.cells.length; i++) {
+			for(var j: number = 0; j < board.cells[0].length; j++) {
+				if(board.getAt(i, j)) filled++;
+			}
+		}
+
+		return filled === 84;
 	}
 
 }
