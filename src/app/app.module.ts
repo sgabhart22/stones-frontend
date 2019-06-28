5f3d971a06c06de97ba62e251478d483e2024349
Spencer C. Gabhart <sgabhart2230@gmail.com>
2019-06-28 08:21:33 -0400

    Typo fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { ClipboardModule } from 'ngx-clipboard';
