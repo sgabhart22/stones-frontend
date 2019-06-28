fa91a51875e07bb81782b6f4525a9351be04babd
Spencer C. Gabhart <sgabhart2230@gmail.com>
2019-06-28 08:01:02 -0400

    Added material modules to app, styled global html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { ClipboardModule } from 'ngx-clipboard';
+
+import { MatDialogModule,
+					MatFormFieldModule,
+					MatInputModule,
+					MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { CellComponent } from './components/cell/cell.component';
 import { BoardComponent } from './components/board/board.component';
 import { GameComponent } from './components/game/game.component';
 import { SaveComponent } from './components/save/save.component';
+import { LoadComponent, LoadDialog } from './components/load/load.component';
 
 @NgModule({
   declarations: [
@@ -15,13 +22,21 @@ import { SaveComponent } from './components/save/save.component';
     CellComponent,
     BoardComponent,
     GameComponent,
-    SaveComponent
+    SaveComponent,
+    LoadComponent,
+		LoadDialog
   ],
   imports: [
     BrowserModule,
-		ClipboardModule
+		ClipboardModule,
+		MatDialogModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule,
+		BrowserAnimationsModule
   ],
   providers: [],
+	entryComponents: [LoadDialog],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
