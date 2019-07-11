144df7c288e63e13167701074102051f9403d366
Spencer C. Gabhart <sgabhart2230@gmail.com>
2019-07-11 16:26:14 -0400

    Added score calculation function

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -13,4 +13,34 @@ const colormap = {
 	}
 };
 
-export { colormap };
+function calcScore(matchDefs: any) {
+	let pts = 0;
+
+	if(matchDefs['classA'] === 0) {
+		pts = 2;
+	} else if(matchDefs['classA'] === 1) {
+		if(matchDefs['classB'] === 0) {
+			pts = 4;
+		} else {
+			pts = 6;
+		}
+	} else if(matchDefs['classA'] === 2) {
+		if(matchDefs['classB'] === 2) {
+			pts = 15;
+		} else {
+			pts = 9;
+		}
+	} else if(matchDefs['classA'] === 3) {
+		if(matchDefs['classB'] === 0) {
+			pts = 15;
+		} else {
+			pts = 20;
+		}
+	} else if(matchDefs['classA'] === 4) {
+		pts = 30;
+	}
+
+	return pts;
+}
+
+export { colormap, calcScore };
